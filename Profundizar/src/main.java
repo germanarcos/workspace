2e7d9293692298a35995964f23934682e1c79af7
German Arcos <arcosgerman28@gmail.com>
2017-11-23 20:29:51 +0100

    23/11

---
+++ Profundizar/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ej2.ej2();
+
+	}
+
+}
---
+++ switch/src/prueba.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String frase = sc.next();
+		switch(frase.length()){
+		default: System.out.println("a");
+		case 4: System.out.println("a");
+		case 3: System.out.println("a");
+		case 2: System.out.println("a");
+		case 1: System.out.println("a");
+		}
+	}
+
+}
